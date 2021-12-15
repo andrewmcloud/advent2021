e5a4672e47070f7c3f9fb026515946620135cd17
Andrew McLoud <amcloud@gmail.com>
2021-12-15 10:17:24 -0500

    day 10, part 2

--- day10.py
+++ day10.py
@@ -4,9 +4,10 @@ with open('resources/day10_input.txt') as f:
 
 matches = {"(": ")", "[": "]", "{": "}", "<": ">"}
 open = ["(", "[", "{", "<"]
-scores = {")": 3, "]": 57, "}": 1197, ">": 25137}
+syntax_checker_scores = {")": 3, "]": 57, "}": 1197, ">": 25137}
 
 illegal_characters = []
+illegal_lines = []
 for line in navigation:
     stack = []
     for c in line:
@@ -15,8 +16,33 @@ for line in navigation:
         else:
             if c != matches.get(stack.pop(-1)):
                 illegal_characters.append(c)
+                illegal_lines.append(line)
                 break
 score = 0
 for ch in illegal_characters:
-    score += scores.get(ch)
+    score += syntax_checker_scores.get(ch)
 print(score)
+
+# part 2
+autocomplete_scores = {")": 1, "]": 2, "}": 3, ">": 4}
+
+incomplete = set(navigation) - set(illegal_lines)
+remaining = []
+for line in incomplete:
+    stack = []
+    for c in line:
+        if c in open:
+            stack.append(c)
+        else:
+            stack.pop(-1)
+    remaining.append(stack)
+
+scores = []
+for line in remaining:
+    score = 0
+    while line:
+        c = line.pop(-1)
+        score = (5 * score) + autocomplete_scores.get(matches.get(c))
+    scores.append(score)
+
+print(sorted(scores)[len(scores)//2])

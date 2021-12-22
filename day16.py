b662f3608c2a20eda4a9349983681baa01c2aa3e
Andrew McLoud <amcloud@gmail.com>
2021-12-21 20:24:59 -0500

    day 16, part 2

--- day16.py
+++ day16.py
@@ -1,6 +1,11 @@
+from math import prod
 with open("resources/day16_input.txt") as f:
     transmission = f.readline().strip()
+
 total = 0
+
+type_fns = {0: sum, 1: prod, 2: min, 3: max, 5: lambda x: int(x[0] > x[1]),
+            6: lambda x: int(x[0] < x[1]), 7: lambda x: int(x[0] == x[1])}
 
 
 def convert_transmission_to_binary(transmission):
@@ -15,45 +20,52 @@ def convert_transmission_to_binary(transmission):
 
 def parse_literal(packets):
     literal = ""
-    prefix, packets = packets[:1], packets[1:]
-    while prefix == "1":
-        packet, prefix, packets = packets[:4], packets[4:5], packets[5:]
-        literal += packet
-    literal += packets[:4]
-    remaining = packets[4:]
-    while remaining[:1] == 0:
-        remaining = remaining[1:]
-    return parse_packets(remaining)
+    index = 6
+    while True:
+        literal += packets[index+1: index+5]
+        if packets[index] == "0":
+            break
+        index += 5
+    index += 5
+    return int(literal, 2), index
 
 
 def parse_operator(packets):
-    prefix, packets = packets[:1], packets[1:]
-    if prefix == "0":
-        num, remaining = int(packets[:15], 2), packets[15:]
-        to_parse = remaining[:num]
-        remaining = remaining[num:]
-        parse_packets(to_parse)
-    else: # prefix == "1"
-        count, remaining = int(packets[:11], 2), packets[11:]
-        while count > 0:
-            remaining = parse_packets(remaining)
-            count -= 1
-    return parse_packets(remaining)
+    index = 6
+    sub_packet = []
+    if packets[index] == "0":
+        length = int(packets[index + 1:index + 1 + 15], 2) + 22
+        index = index + 1 + 15
+        while index < length:
+            literal, bit_pos = parse_packets(packets[index:])
+            index += bit_pos
+            sub_packet.append(literal)
+    else: # packets[index] = "1"
+        count = int(packets[index + 1: index + 1 + 11], 2)
+        index = index + 1 + 11
+        for _ in range(count):
+            literal, bit_pos = parse_packets(packets[index:])
+            index += bit_pos
+            sub_packet.append(literal)
+
+    return sub_packet, index
 
 
 def parse_packets(packets):
     global total
-    if not packets:
-        return
-    if int(packets, 2) == 0:
-        return
-    version, p_type, packets = packets[:3], packets[3:6], packets[6:]
-    total += (int(version, 2))
-    if int(p_type, 2) == 4:
+    version, packet_type = int(packets[:3], 2), int(packets[3:6], 2)
+    total += version
+    if packet_type == 4:
         return parse_literal(packets)
     else:
-        return parse_operator(packets)
+        numbers, index = parse_operator(packets)
+    result = 0
+    if packet_type in type_fns:
+        result = type_fns.get(packet_type)(numbers)
+    return result, index
 
+# part 2
+print(parse_packets(convert_transmission_to_binary(transmission))[0])
 
-parse_packets(convert_transmission_to_binary(transmission))
+# part 1
 print(total)
